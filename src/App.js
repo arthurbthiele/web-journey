53c713fb1b203e0e5c5f9d88f8f9d4a565e27424
Arthur Thiele <arthurbthiele@gmail.com>
2021-07-22 17:46:15 +1000

    Removing unnecessary code

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ import {
 } from "./components";
 
 function App() {
-  // const [graph, setGraph] = useState({
-  //   nodes: [{ id: "a", label: "a" }],
-  //   edges: [],
-  // });
-  // const depths = useRef({}).current;
-
-  // updateDepths(depths, graph.nodes);
-
   return (
     <PageContainer>
       <GraphProvider>
